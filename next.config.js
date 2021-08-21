3911329f985184a91bb0250f93412c1224a7bf00
chen hong <vvchennnnn@gmail.com>
2021-08-21 13:01:32 +1000

    mapbox

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
     domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoidnZjaGVuIiwiYSI6ImNrc2wycThjaTJxY2Uyem5xZ3VsZmVyaWsifQ.m3e0gfJ7Cri7IhFDbp9Hzg'
   }
 }
